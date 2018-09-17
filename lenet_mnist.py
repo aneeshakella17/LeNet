bf1981a01b267348b14535a5efeee11f9c0f54b9
mac <mac@macs-MacBook-Pro-2.local>
2018-09-17 16:28:46 -0700

    fourth

--- lenet_mnist.py
+++ lenet_mnist.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn import datasets
+import matplotlib.pyplot as plt
 from keras import backend as K
 import numpy as np
 
