dc7f3589660336da11e930700ad5726d1b07282f
mac <mac@macs-MacBook-Pro-2.local>
2018-09-17 16:58:00 -0700

    7

--- lenet_mnist.py
+++ lenet_mnist.py
@@ -53,4 +53,3 @@ predictions = model.predict(testX, batch_size=128)
 print(classification_report(testY.argmax(axis=1),
 	predictions.argmax(axis=1),
 	target_names=[str(x) for x in le.classes_]))
-model.save("my_file.hdf5")
